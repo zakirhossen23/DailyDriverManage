ad991abeef59fd42fde02968b3bad76ac643b162
Zakir Hossen <73053667+zakirhossen23@users.noreply.github.com>
2021-12-11 10:58:00 +0600

    Initial commit

---
+++ src/dailydrivermanage/DailyDriverManage.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dailydrivermanage;
+
+/**
+ *
+ * @author zakir
+ */
+public class DailyDriverManage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       MainFrame mainframe = new MainFrame();           //initializing MainFrame 
+       mainframe.setVisible(true);                      //showing MainFrame/Main Gui Interface
+       
+    }
+    
+}
